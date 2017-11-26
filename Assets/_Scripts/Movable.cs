fb4851dfdfe9672da1e935ff2f0f616b0dc0520e
Michael Manley <mmanle01@live.com>
2017-11-26 11:37:21 -0500

    Initial Commit. Has basic working Movable system.

---
+++ Assets/_Scripts/Movable.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movable : MonoBehaviour
+{
+    [SerializeField] private float gridSize = 1f;
+    [SerializeField] private float rotationSensitivity = 5f;
+
+    private Vector3 initialMousePosition;
+    private Vector3 offset;
+    private MovementManager manager;
+
+    public bool IsPlacing
+    {
+        get;
+        protected set;
+    }
+
+    public bool IsRotating
+    {
+        get;
+        protected set;
+    }
+
+    public void FollowMouse()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (manager.snapToGrid)
+            {
+                var x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+                var y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+                var pos = new Vector3(x, y, 0); //Where the game object SHOULD be without snapping
+
+                int gridStep = Mathf.RoundToInt(pos.x / gridSize);
+                pos.x = ((float)gridStep) * gridSize;
+
+                gridStep = Mathf.RoundToInt(pos.y / gridSize);
+                pos.y = ((float)gridStep * gridSize);
+
+                gameObject.transform.position = pos;//After snapping
+            }
+            else
+            {
+                var x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+                var y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+                gameObject.transform.position = new Vector3(x, y, 0) + offset;
+            }
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            IsPlacing = false;
+        }
+    }
+
+    //Add Keyboard Input here and put below FollowMouse in Update method.
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsPlacing)
+        {
+            FollowMouse();
+        }
+        else if (IsRotating)
+        {
+            ChangeRotation();
+        }
+    }
+
+    private void ChangeRotation()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            float angle = initialMousePosition.x - Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+            transform.Rotate(Vector3.forward, angle * sensitivity);
+            initialMousePosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        }
+        else
+        {
+            IsRotating = false;
+        }
+    }
+
+    public void SetMovable(Vector3 rayHitLocation)
+    {
+        offset = transform.position - rayHitLocation;
+        IsPlacing = true;
+        IsRotating = false;
+    }
+
+    public void SetRotating(Vector3 rayHitLocation)
+    {
+        initialMousePosition = rayHitLocation;
+        IsRotating = true;
+        IsPlacing = false;
+    }
+
+    private void Awake()
+    {
+        if (manager == null)
+        {
+            manager = GameObject.FindGameObjectWithTag("MovementManager").GetComponent<MovementManager>();
+        }
+    }
+
+}
---
+++ Assets/_Scripts/MovementManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    private Movable currentMovingObject = null;
+    public bool snapToGrid = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentMovingObject != null && currentMovingObject.IsPlacing == true) //We are currently Placing, don't need to raycast.
+        {
+            return;
+        } 
+        else if (currentMovingObject != null && currentMovingObject.IsPlacing == false)// no longer placing. Forget about the last object and continue raycasting.
+        {
+            currentMovingObject = null;
+        }
+
+        RayCastForMovableObject();
+
+    }
+    private void RayCastForMovableObject()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                                    Camera.main.ScreenToWorldPoint(Input.mousePosition).y),
+                                                    Vector2.zero, 0f);
+            if (hit.transform != null)
+            {
+                var movable = hit.transform.GetComponentInParent<Movable>();
+                if (movable != null)
+                {
+                    currentMovingObject = movable;
+                    movable.SetMovable(hit.point);
+                }
+                else
+                {
+                    Debug.Log("Click didn't land on a movable object");
+                }
+            }
+
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            var hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                                    Camera.main.ScreenToWorldPoint(Input.mousePosition).y),
+                                                    Vector2.zero, 0f);
+            if (hit.transform != null)
+            {
+                var movable = hit.transform.GetComponentInParent<Movable>();
+                if (movable != null)
+                {
+                    currentMovingObject = movable;
+                    movable.SetRotating(hit.point);
+                }
+                else
+                {
+                    Debug.Log("Click didn't land on a movable object");
+                }
+            }
+        }
+
+    }
+
+}
